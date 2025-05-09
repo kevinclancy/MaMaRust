263e4651abffc3ed22a5927915945786f9151cca
Kevin Clancy <kclanc@gmail.com>
2025-05-09 12:05:29 -0700

    implemented the rest of the instructions

--- src/heap.rs
+++ src/heap.rs
@@ -8,6 +8,9 @@ const TAG_FUNCTION : u8 = 0x02;
 const TAG_VECTOR : u8 = 0x03;
 const TAG_REF : u8 = 0x04;
 const TAG_SUM : u8 = 0x05;
+/// NOTE: we don't use vectors to represent tuples because they can't be guaranteed to take
+/// less memory than a closure, which makes rewrite impossible
+const TAG_TUPLE : u8 = 0x06;
 
 pub struct Heap {
   next_addr : usize,
@@ -53,11 +56,40 @@ impl Heap {
     }
   }
 
+  ///
+  pub fn rewrite(&mut self, src: HeapAddr, target: HeapAddr) {
+    let target_addr = usize::try_from(target).unwrap();
+    assert!(self.data[target_addr] == TAG_CLOSURE);
+    // we can write differing amounts depending on the type of src, or just write 13 bytes (size of closure)
+    // the performance impact should be studied
+    let target : usize = src.try_into().unwrap();
+    let src : usize = target.try_into().unwrap();
+    // closure objects are 13 bytes, so that is how many we copy
+    self.data[target] = self.data[src];
+    self.data[target + 1] = self.data[src + 1];
+    self.data[target + 2] = self.data[src + 2];
+    self.data[target + 3] = self.data[src + 3];
+    self.data[target + 4] = self.data[src + 4];
+    self.data[target + 5] = self.data[src + 5];
+    self.data[target + 6] = self.data[src + 6];
+    self.data[target + 7] = self.data[src + 7];
+    self.data[target + 8] = self.data[src + 8];
+    self.data[target + 9] = self.data[src + 9];
+    self.data[target + 10] = self.data[src + 10];
+    self.data[target + 11] = self.data[src + 11];
+    self.data[target + 12] = self.data[src + 12];
+  }
+
   pub fn expect_sum(&mut self, addr: HeapAddr) -> (u8, HeapAddr) {
     let addr = usize::try_from(addr).unwrap();
     assert!(self.data[addr] == TAG_SUM);
     let args_addr = i32::from_le_bytes([self.data[addr+2], self.data[addr+3], self.data[addr+4], self.data[addr+5]]);
     (self.data[addr+1], args_addr)
+  }
+
+  pub fn is_closure(&self, addr: HeapAddr) -> bool {
+    let addr = usize::try_from(addr).unwrap();
+    self.data[addr] == TAG_CLOSURE
   }
 
   pub fn expect_closure(&mut self, addr: HeapAddr) -> (CodeAddr, HeapAddr) {
@@ -93,6 +125,13 @@ impl Heap {
     res
   }
 
+  pub fn expect_ref(&mut self, addr: HeapAddr) -> HeapAddr {
+    let addr = usize::try_from(addr).unwrap();
+    assert!(self.data[addr] == TAG_REF);
+    let content_addr = i32::from_le_bytes([self.data[addr + 1], self.data[addr + 2], self.data[addr + 3], self.data[addr + 4]]);
+    content_addr
+  }
+
   pub fn expect_basic(&mut self, addr: HeapAddr) -> i32 {
     let addr = usize::try_from(addr).unwrap();
     assert!(self.data[addr] == TAG_BASIC);
@@ -123,20 +162,22 @@ impl Heap {
     self.write_u8(TAG_CLOSURE);
     self.write_i32(code_addr);
     self.write_i32(global_vec);
+    // write an additional 32-bit word to ensure that closures occupy as much space a functions (enabling rewrite)
+    self.write_i32(0);
     ret.try_into().unwrap()
   }
 
-  pub fn new_basic(&mut self, n : u32) -> HeapAddr {
+  pub fn new_basic(&mut self, n : i32) -> HeapAddr {
     let ret = self.next_addr;
     self.write_u8(TAG_BASIC);
-    self.write_u32(n);
+    self.write_i32(n);
     ret.try_into().unwrap()
   }
 
-  pub fn new_ref(&mut self, n : u32) -> HeapAddr {
+  pub fn new_ref(&mut self, n : i32) -> HeapAddr {
     let ret = self.next_addr;
     self.write_u8(TAG_REF);
-    self.write_u32(n);
+    self.write_i32(n);
     ret.try_into().unwrap()
   }
 
--- src/virtual_machine.rs
+++ src/virtual_machine.rs
@@ -94,9 +94,23 @@ impl VirtualMachine {
       sp -= 1;
     };
 
+    let slide = |n : i32| {
+      s[sp.try_into().unwrap() - n] = s[sp];
+      sp = sp.try_into().unwrap() - n;
+    };
+
+    let rewrite = |n : i32| {
+      heap.rewrite(s[sp.try_into().unwrap() - n],s[sp]);
+      sp -= 1;
+    };
+
+    let pushloc = |n : i32| {
+      s[sp + 1] = s[sp.try_into().unwrap() - n];
+    };
+
     loop {
       let instr = self.instructions[pc];
-        match (instr & 0x00000011) {
+        match instr & 0x000000FF {
             0x00 => break, // halt
             0x01 => { // mul
               s[sp - 1] = s[sp - 1] * s[sp];
@@ -151,7 +165,7 @@ impl VirtualMachine {
               let (variant_id, _) = heap.expect_sum(s[sp]);
               let instr_bytes = instr.to_le_bytes();
               let jump_table_addr = u16::from_le_bytes([instr_bytes[1], instr_bytes[2]]);
-              pc = Into::<usize>::into(jump_table_addr) + Into::<usize>::into(variant_id);
+              pc = Into::<i32>::into(jump_table_addr) + Into::<i32>::into(variant_id);
             },
             0x0C => { // TGetConstructorArg
               let (_, args_vec_addr) = heap.expect_sum(s[sp]);
@@ -159,12 +173,174 @@ impl VirtualMachine {
               s[sp] = args_vec_addr;
               pc += 1;
             },
-            0x0D => { // Eval
-
-            }
-
-
-            _ => panic!("todo")
+            0x0D => { // Pop
+              sp -= 1;
+              pc += 1;
+            },
+            0x0E => { // GetRef
+              let n = heap.expect_ref(s[sp]);
+              s[sp] = n;
+              pc = pc + 1;
+            },
+            0x0F => { // MkRef
+              s[sp] = heap.new_ref(s[sp]);
+              pc += 1;
+            },
+            0x10 => { // RefAssign
+              let _ = heap.expect_ref(s[sp]);
+              // TODO TODO TODO !!! heap.new_ref()
+            },
+            0x11 => { // GetBasic
+              let n = heap.expect_basic(s[sp]);
+              s[sp] = n;
+              pc += 1;
+            },
+            0x12 => { // MkBasic
+              s[sp] = heap.new_basic(s[sp]);
+              pc += 1;
+            },
+            0x13 => { // PushLoc(n)
+              let n : i32 = (instr & 0x00FFFF00) >> 8;
+              pushloc(n);
+              pc += 1;
+            },
+            0x14 => { // PushGloba(n)
+              let n : i32 = (instr & 0x00FFFF00) >> 8;
+              let globals  = heap.expect_vector(gp);
+              if n < globals.len() {
+                s[sp + 1] = globals[n];
+                sp += 1;
+                pc += 1;
+              } else {
+                panic!("fewer than n globals");
+              }
+            },
+            0x15 => { // Slide(n)
+              let n : i32 = (instr & 0x00FFFF00) >> 8;
+              slide(n);
+              pc += 1;
+            },
+            0x16 => { // GetVec
+              let elems = heap.expect_vector(s[sp]);
+              for i in 0..elems.len() {
+                s[sp + i] = elems[i];
+              }
+              sp = sp + elems.len() - 1;
+              pc += 1;
+            },
+            0x17 => { // MkVec(n)
+              let n : i32 = (instr & 0x00FFFF00) >> 8;
+              let vec = vec![0; n];
+              sp = sp - n + 1;
+              for i in 0..n {
+                vec[i] = s[sp + i];
+              };
+              s[sp] = heap.new_vector(&vec[..]);
+              pc += 1;
+            },
+            0x18 => { // MkFunVal(addr)
+              let code_addr : i32 = (instr & 0x00FFFF00) >> 8;
+              let args_addr = heap.new_vector(&[0;0]);
+              s[sp] = heap.new_function(code_addr, args_addr, s[sp]);
+              pc += 1;
+            },
+            0x19 => { // MkClos(addr)
+              let code_addr : i32 = (instr & 0x00FFFF00) >> 8;
+              s[sp] = heap.new_closure(code_addr, s[sp]);
+              pc += 1;
+            },
+            0x1A => { // Mark(return_addr)
+              let return_addr : i32 = (instr & 0x00FFFF00) >> 8;
+              mark(return_addr);
+              pc += 1;
+            },
+            0x1B => { // Apply
+              apply ()
+            },
+            0x1C => { // TArg(numFormals)
+              let num_formals : i32 = (instr & 0x0000FF00) >> 8;
+              if sp - fp < num_formals {
+                mkvec0 ();
+                wrap ();
+                popenv ()
+              } else {
+                pc += 1;
+              }
+            },
+            0x1D => { // Return(numFormals)
+              let num_formals : i32 = (instr & 0x0000FF00) >> 8;
+              if sp - fp - 1 == num_formals {
+                popenv ()
+              } else {
+                slide(num_formals);
+                apply();
+              }
+            },
+            0x1E => { // Alloc(num_closures_to_alloc)
+              let num_closures_to_alloc : i32 = (instr & 0x0000FF00) >> 8;
+              for i in 1..(num_closures_to_alloc + 1) {
+                s[sp + i] = heap.new_closure(-1, -1);
+              }
+              sp += num_closures_to_alloc;
+              pc += 1;
+            },
+            0x1F => { // Rewrite(n)
+              let n : i32 = (instr & 0x0000FF00) >> 8;
+              heap.rewrite(s[sp], s[sp-n]);
+              sp -= 1;
+              pc += 1;
+            },
+            0x20 => { // Eval
+              match heap.is_closure(s[sp]) {
+                true => {
+                  mark(pc + 1);
+                  pushloc(3);
+                  apply0();
+                },
+                false => {
+                  pc = pc + 1;
+                }
+              }
+            },
+            0x21 => { // Update
+              popenv();
+              rewrite(1);
+            },
+            0x22 => { // Load(numWords)
+              let num_words_to_load : i32 = (instr & 0x0000FF00) >> 8;
+              for i in (0..num_words_to_load).rev() {
+                s[sp + i] = s[s[sp].try_into().unwrap() + i];
+              }
+              sp = sp + num_words_to_load - 1;
+              pc += 1;
+            },
+            0x23 => { // LoadC(constantToLoad)
+              let constant_to_load : i32 = (instr & 0xFFFFFF00) >> 8;
+              sp += 1;
+              s[sp] = constant_to_load;
+              pc += 1;
+            },
+            0x24 => { // Jump(destAddr)
+              let dest_addr : i32 = (instr & 0x00FFFF00) >> 8;
+              pc = dest_addr;
+            },
+            0x25 => { // JumpZ(destAddr)
+              let dest_addr : i32 = (instr & 0x00FFFF00) >> 8;
+              pc = if s[sp] == 0 { dest_addr } else { pc + 1 };
+              sp -= 1;
+            },
+            0x26 => { // JumpNZ(destAddr)
+              let dest_addr : i32 = (instr & 0x00FFFF00) >> 8;
+              let n = heap.expect_basic(s[sp]);
+              pc = if n != 0 { dest_addr } else { pc + 1 };
+              sp -= 1;
+            },
+            0x27 => { // JumpI(jump_offset)
+              let jump_offset : i32 = (instr & 0x00FFFF00) >> 8;
+              pc = s[sp] + jump_offset;
+              sp -= 1;
+            },
+            _ => panic!("invalid instruction")
         }
     }
   }
